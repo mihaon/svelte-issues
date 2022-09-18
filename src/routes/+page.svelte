f35fc6fb5c374114d50252e2961573317f1c4b3a
Mihail Haritonov <mail@m-haritonov.net>
2022-09-18 20:17:39 +0300

    #6875 <link> and <meta> inside <svelte:head> are duplicated in SSR generated HTML when bind:value is present

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<link rel="canonical" href="/test">
+	<meta name="description" content="test">
+</svelte:head>
+<script>
+	import Foo from '$lib/Foo.svelte'
+	let bar
+</script>
+<Foo bind:bar/>
